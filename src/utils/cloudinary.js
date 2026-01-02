46bd41eb7789dddffb5d4b0189ce7a1e95d5f1c9
snxhaSalgaonkar <snehasalgaonkar.012@gmail.com>
2026-01-02 20:26:18 +0530

    10.  how to upload file in backend using cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,89 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localfilepath) => {
+  try {
+    if (!localfilepath) return null;
+    //upload the file on cloudinary
+    const uploadResult = await cloudinary.uploader.upload(localfilepath, {
+      resource_type: "auto",
+    });
+    //file has been uploaded,
+    console.lpg("***********************");
+    console.log("File uploaded on cloudinary successfully:", uploadResult);
+    console.log("file url", uploadResult.url);
+    return uploadResult;
+  } catch (error) {
+    console.log(error);
+    fs.unlinkSync(localfilepath); // remove
+    // the locally stored tomparailly file
+    //  as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
+
+// (async function () {
+//   Configuration
+//   cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+//   });
+
+//   const uploadOnCloudinary = async (localfilepath) => {
+//     try {
+//       if (!localfilepath) return null;
+//       //upload the file on cloudinary
+//       const uploadResult = await cloudinary.uploader.upload(localfilepath, {
+//         resource_type: "auto",
+//       });
+//       //file has been uploaded,
+//       console.lpg("***********************");
+//       console.log("File uploaded on cloudinary successfully:", uploadResult);
+//       console.log("file url", uploadResult.url );
+//       return uploadResult;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   // Upload an image
+//   const uploadResult = await cloudinary.uploader
+//     .upload(
+//       "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+//       {
+//         public_id: "shoes",
+//       },
+//     )
+//     .catch((error) => {
+//       console.log(error);
+//     });
+
+//   console.log(uploadResult);
+
+//   // Optimize delivery by resizing and applying auto-format and auto-quality
+//   const optimizeUrl = cloudinary.url("shoes", {
+//     fetch_format: "auto",
+//     quality: "auto",
+//   });
+
+//   console.log(optimizeUrl);
+
+//   // Transform the image: auto-crop to square aspect_ratio
+//   const autoCropUrl = cloudinary.url("shoes", {
+//     crop: "auto",
+//     gravity: "auto",
+//     width: 500,
+//     height: 500,
+//   });
+
+//   console.log(autoCropUrl);
+// })();
